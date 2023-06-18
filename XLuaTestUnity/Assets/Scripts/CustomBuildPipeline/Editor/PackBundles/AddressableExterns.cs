04069a9d8cc36222e741f6be64eb04b546eaf979
chenxufeng <chenxufeng@xiaoice.com>
2023-06-18 22:20:48 +0800

    添加Addressable的扩展，创建程序集引用

---
+++ XLuaTestUnity/Assets/Scripts/CustomBuildPipeline/Editor/PackBundles/AddressableExterns.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+// using Newtonsoft.Json;
+
+public class AddressableExterns
+{
+	public static Dictionary<string, string> _aaMap;
+	private static string _mapFilePath = Application.dataPath + "/AddressableAssetsData/aaMap.json";
+	
+	[UnityEditor.InitializeOnLoadMethod]
+	public static void InitFunc()
+	{
+		Debug.Log("init aaMap");
+		_mapFilePath = Application.dataPath + "/AddressableAssetsData/aaMap.json";
+		_aaMap = new Dictionary<string, string>();
+		LoadMap();
+	}
+	
+	public static void ResetMap()
+	{
+		Debug.Log("Reset aaMap");
+		if(_aaMap == null)
+			_aaMap = new Dictionary<string, string>();
+		_aaMap.Clear();
+	}
+
+	private static void AddOrUpdate(string key,string value)
+	{
+		if(!_aaMap.ContainsKey(key))
+			_aaMap.Add(key,value);
+		else
+		{
+			Debug.Log($"_dic contains same key is {key} old value is {_aaMap[key]} new value is {value}");
+		}
+	}
+	public static void UpdateBundleMap(string source,string output,string GroupName)
+	{
+		var len = source.Length;
+		var name = source.Substring(7,len-14);
+		if (name == "all")
+		{
+			Debug.Log($"originName {source} bundleResultInfo.SourceAssetGroup {GroupName}");
+			name = "Group_" + GroupName;
+		}
+		AddOrUpdate(name,output);
+	}
+
+	public static string GetBundleName(string key)
+	{
+		if (_aaMap == null)
+		{
+			Debug.LogError("_aaMap is null,please Rebuilt aa");
+			return "";
+		}
+
+		if (_aaMap.ContainsKey(key))
+			return _aaMap[key];
+		else
+		{
+			//Debug.LogWarning($"_aaMap is not contains key {key},please Rebuilt aa");
+			return "";
+		}
+	}
+
+	public static void ClearAll()
+	{
+		_aaMap.Clear();
+	}
+
+	public static void SaveFile()
+	{
+		// string result = JsonConvert.SerializeObject(_aaMap);
+		// File.WriteAllText(_mapFilePath,result);
+	}
+	public static bool LoadMap()
+	{
+		if (!File.Exists(_mapFilePath))
+		{
+			Debug.LogWarning("_mapFilePath is not exists,please Rebuilt aa");
+			return false;
+		}
+		// string json =File.ReadAllText(_mapFilePath);
+		// _aaMap = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+		return true;
+	}
+}
---
+++ XLuaTestUnity/Packages/com.unity.addressables/Editor/Build/GitVersionStat.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+
+namespace UnityEditor.AddressableAssets.Build {
+	public static class GitVersionStat {
+		private static Dictionary<GUID, int> _modifiedAssets;
+
+		public static int CheckAssetIsModified(GUID guid) {
+			if( _modifiedAssets.TryGetValue(guid, out var modifyValue) ) {
+				return modifyValue;
+			}
+
+			return 0;
+		}
+
+		public static void FetchGitLogAndCollect(string startTime = "2023-1-16") {
+			_modifiedAssets = new Dictionary<GUID, int>();
+			var process = new Process();
+			process.StartInfo = new ProcessStartInfo {
+#if UNITY_EDITOR_WIN
+				FileName = "git.exe",
+#else
+				FileName = "git",
+#endif
+				Arguments = $"log --no-merges --name-status --pretty=oneline --since={startTime} -- {Application.dataPath}",
+				CreateNoWindow = true,
+				RedirectStandardOutput = true,
+				UseShellExecute = false
+			};
+			process.Start();
+			var reader = process.StandardOutput;
+			var gitLog = "";
+			while( !process.HasExited ) {
+				gitLog += reader.ReadToEnd();
+			}
+
+			gitLog += reader.ReadToEnd();
+			using( var logReader = new StringReader(gitLog) ) {
+				var logLine = logReader.ReadLine();
+				do {
+					try
+					{
+						if( logLine.EndsWith(".meta") ) {
+							logLine = logLine.Substring(0, logLine.Length - 5);
+						}
+
+						var modifyValue = logLine.StartsWith("M") ? 1 : logLine.StartsWith("A") ? 2 : 0;
+						if( modifyValue > 0 ) {
+							var assetPath = logLine.Substring(2 + 22);
+							var assetGUID = AssetDatabase.AssetPathToGUID(assetPath);
+
+							if( GUID.TryParse(assetGUID, out var guid) ) {
+								if( _modifiedAssets.TryGetValue(guid, out var existModifyValue) ) {
+									if( existModifyValue < modifyValue ) {
+										_modifiedAssets[guid] = modifyValue;
+									}
+								}
+								else {
+									_modifiedAssets.Add(guid, modifyValue);
+								}
+							}
+						}
+
+						logLine = logReader.ReadLine();
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e);
+					//	throw;
+					}
+				} while( !string.IsNullOrEmpty(logLine) );
+			}
+		}
+	}
+}
